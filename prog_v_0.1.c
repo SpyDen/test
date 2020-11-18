bfb2a4edf0e26b8251a1128d8b8762ac5725d836
Denys Spys <spisdenis2001@gamil.com>
2020-11-18 11:47:22 +0200

    Second version

--- prog_v_0.1.c
+++ prog_v_0.1.c
@@ -6,12 +6,43 @@ int main(void)
     double S = 0;
 
     fprintf(stdout, "Please, enter the first leg >> ");
-    fscanf(stdin, "%hd", &a);
+    while (fscanf(stdin, "%hd", &a) != 1 || a <= 0 || getchar() != '\n')
+    {
+        fprintf(stderr, "Sorry, but you have entered incorrect data!\n");
+        while (getchar() != '\n')
+        {
+            continue;
+        }
+        fprintf(stdout, "Please, enter the first leg >> ");
+    }
+
+
     fprintf(stdout, "Please, enter the second leg >> ");
-    fscanf(stdin, "%hd", &b);
+    while (fscanf(stdin, "%hd", &b) != 1 || b <= 0 || getchar() != '\n')
+    {
+        fprintf(stderr, "Sorry, but you have entered incorrect data!\n");
+        while (getchar() != '\n')
+        {
+            continue;
+        }
+        fprintf(stdout, "Please, enter the second leg >> ");
+    }
+
+
     fprintf(stdout, "Please, enter the hypotenuse >> ");
-    fscanf(stdin, "%hd", &c);
+    while (fscanf(stdin, "%hd", &c) != 1 || c <= 0 || getchar() != '\n')
+    {
+        fprintf(stderr, "Sorry, but you have entered incorrect data!\n");
+        while (getchar() != '\n')
+        {
+            continue;
+        }
+        fprintf(stdout, "Please, enter the hypotenuse >> ");
+    }
+
+
     S = a * b / 2;
+
     fprintf(stdout, "The area of this triangle is: %f\n", S);
     return 0;
 }
